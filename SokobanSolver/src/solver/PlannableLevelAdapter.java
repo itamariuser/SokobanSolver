6a6e313184fcf9aca7188dcf0772a2908b93cb59
itama <itama@DESKTOP-9USF134>
2017-06-13 20:58:49 +0300

    4MATAN

--- SokobanSolver/src/solver/PlannableLevelAdapter.java
+++ SokobanSolver/src/solver/PlannableLevelAdapter.java
@@ -15,32 +15,53 @@ import gameObjects.Position2D;
 import model.Level2D;
 import model.SokobanPolicy;
 
-public class PlannableLevelAdapter implements Plannable<Position2D> {
+public class PlannableLevelAdapter implements Plannable<Position2D> {//Generates a new searchable, everytime goalState= where we want to go for "Move player" action
 
 	private SokobanPolicy policy;
 	private Level2D level;
+	AndPredicate<Position2D> kb;
+	public PlannableLevelAdapter() {
+		kb=new AndPredicate<Position2D>();
+	}
 	
+	/**
+	 * return true only if p1 contradicts p2
+	 *  
+	 */
 	@Override
-	public boolean contradicts(Predicate<Position2D> arg0, Predicate<Position2D> arg1) {
-		// TODO Auto-generated method stub
+	public boolean contradicts(Predicate<Position2D> p1, Predicate<Position2D> p2) {
+		
 		return false;
 	}
 	@Override
 	public Predicate<Position2D> getGoal() {
 		AndPredicate<Position2D> pToReturn = new AndPredicate<Position2D>();
-		HashMap<Position2D, ArrayList<GameObject>> layout=level.getPositionObjectLayout();
 		for (GameObject g : level.getObjReferences()) {
 			if(g instanceof GoalPoint)
 			{
 				for (Position2D pos : level.getPositionsOfObject(g)) {
-					
+					pToReturn.add(new SimplePredicate<Position2D>("Crate #?",pos));
 				}
 			}
 		}
+		return pToReturn;
 	}
 	@Override
 	public AndPredicate<Position2D> getKnowledgebase() {
-		// TODO Auto-generated method stub
+		
+		HashMap<Position2D, ArrayList<GameObject>> layout=level.getPositionObjectLayout();
+		for (Position2D pos : layout.keySet()) {
+			for (GameObject gameObj : level.getGameObjectArrayOf(pos)) {
+				kb.add(generatePredicate(gameObj));
+			}
+		}
+		return kb;
+	}
+	
+	private Predicate<Position2D> generatePredicate(GameObject gameObj)//TODO: generate predicate for each game object
+	{
+		//Rules rules rules....
+		//if instanceof crate, then add crate etc. etc.
 		return null;
 	}
 	@Override
